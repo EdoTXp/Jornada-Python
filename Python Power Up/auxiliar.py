a6f7acb46b2302a1573e40592e00e7d6deecbda7
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2023-07-26 11:44:56 -0300

    refactoring the code

--- Python Power Up/auxiliar.py
+++ Python Power Up/auxiliar.py
@@ -1,5 +1,6 @@
 import time
 import pyautogui
 
+# Pegando a posição do mouse e printando na tela depois de um intervalo de 5 segundos
 time.sleep(5)
 print(pyautogui.position())
--- Python Power Up/main.py
+++ Python Power Up/main.py
@@ -2,52 +2,51 @@ import pandas as pd
 import time
 import pyautogui
 
-
+# Setando o intervalo padrão entre as ações do pyautogui
 pyautogui.PAUSE = 0.5
+tempo_espera = 2
 
-# Abrindo o chrome e entrando no site
+# Abrindo o chrome
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
 
-pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
-pyautogui.press("enter")
-time.sleep(2)
+# Entrando no site
+url = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 
-# logando
-pyautogui.click(x=940, y=396)
-pyautogui.write("email@hotmail.com")
+pyautogui.write(url)
+pyautogui.press("enter")
+time.sleep(tempo_espera)
+
+# Logando
+email = "email@hotmail.com"
+senha = "JCQmpBg$$5EN08Oj0Lb4Ae"
+
+pyautogui.click(x=940, y=396) # clicando o primeiro campo do email. OBS: Mudar as coordenadas caso a resolução do monitor é diferente
+pyautogui.write(email)
 pyautogui.press("tab")
-pyautogui.write("JCQmpBg$$5EN08Oj0Lb4Ae")
+pyautogui.write(senha)
 pyautogui.press("tab")
 pyautogui.press("enter")
+time.sleep(tempo_espera)
 
-#cadastrando os dados
-tabela = pd.read_csv("F:\Progetti\Visual Studio Code\Python\Jornada Python\Python Power Up\produtos.csv")
+# Cadastrando os dados
+percurso = "F:\Progetti\Visual Studio Code\Python\Jornada Python\Python Power Up\produtos.csv"
+
+tabela = pd.read_csv(percurso)
 
 for linha in tabela.index:
+    # Voltar ao início da tela e clicar no primeiro campo para inserir os dados
+    pyautogui.press("pageup")
     pyautogui.click(x=831, y=271)
     
-    pyautogui.write(str(tabela.loc[linha, "codigo"]))
+    for coluna in tabela.columns:
+        if coluna == "obs":
+            if not pd.isna(tabela.loc[linha, coluna]): pyautogui.write(str(tabela.loc[linha, coluna]))
+            break
+        
+        pyautogui.write(str(tabela.loc[linha, coluna]))
+        pyautogui.press("tab")
+        
     pyautogui.press("tab")
-    
-    pyautogui.write(str(tabela.loc[linha, "marca"]))
-    pyautogui.press("tab")
-    
-    pyautogui.write(str(tabela.loc[linha, "tipo"]))
-    pyautogui.press("tab")
-    
-    pyautogui.write(str(tabela.loc[linha, "categoria"]))
-    pyautogui.press("tab")
-    
-    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
-    pyautogui.press("tab")
-    
-    pyautogui.write(str(tabela.loc[linha, "custo"]))
-    pyautogui.press("tab")
-    
-    if not pd.isna(tabela.loc[linha, "obs"]):
-        pyautogui.write(str(tabela.loc[linha, "obs"]))
-    pyautogui.press("tab")
-    
     pyautogui.press("enter")
